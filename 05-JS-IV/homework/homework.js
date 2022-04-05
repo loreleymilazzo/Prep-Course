89dc6a821014c951c3ee86933f5ba66c6c8049c0
loreleymilazzo <clm12343@gmail.com>
2022-04-05 11:46:32 -0300

    homework 05 js

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -6,7 +6,14 @@ function crearGato(nombre, edad) {
   // Agrega un método (funcion) llamado "meow" que devuelva el string "Meow!"
   // Devuelve el objeto
   // Tu código:
-
+ var obj = {
+   nombre: nombre,
+   edad: edad,
+   meow: function meow(){
+     return "Meow!"
+   }
+ } 
+ return obj
 }
 
 
@@ -15,7 +22,8 @@ function agregarPropiedad(objeto, property) {
   // Devuelve el objeto
   // NOTA: El nombre de la propiedad no es "propiedad", el nombre es el valor del argumento llamado "property" (una cadena/string)
   // Tu código:
- 
+  objeto[property] = null;
+  return objeto;
 }
 
 function invocarMetodo(objeto, metodo) {
@@ -23,6 +31,7 @@ function invocarMetodo(objeto, metodo) {
   // Invoca ese método
   // Nada necesita ser devuelto ("returned")
   // Tu código:
+ objeto[metodo]();
  
 }
 
@@ -31,6 +40,7 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
 
+  return objetoMisterioso.numeroMisterioso * 5;
 }
 
 function eliminarPropiedad(objeto, unaPropiedad) {
@@ -38,6 +48,9 @@ function eliminarPropiedad(objeto, unaPropiedad) {
   // tip: tenes que usar bracket notation
   // Devuelve el objeto
   // Tu código:
+
+ delete objeto[unaPropiedad];
+ return objeto 
   
 }
 
@@ -46,18 +59,25 @@ function nuevoUsuario(nombre, email, password) {
   // Devuelve el objeto
   // Tu código:
  
-
-
+var objet = {
+  nombre: nombre,
+  email: email,
+  password: password
+}
+return objet
 }
 
 function tieneEmail(usuario) {
   // Devuelve "true" si el usuario tiene un valor definido para la propiedad "email"
   // De lo contratio, devuelve "false"
   // Tu código:
-  
+  if (usuario.email) {
+    return true
+  }else {
+    return false 
+  }
   
 }
-
 
 
 function tienePropiedad(objeto, propiedad) {
@@ -65,7 +85,7 @@ function tienePropiedad(objeto, propiedad) {
   // "propiedad" es un string
   // De lo contrario, devuelve "false"
   // Tu código:
-
+return objeto.hasOwnProperty(propiedad);
 
 
 }
@@ -75,6 +95,11 @@ function verificarPassword(usuario, password) {
   // Devuelve "true" si coinciden
   // De lo contrario, devuelve "false"
   // // Tu código:
+  if(password === usuario.password){
+    return true
+  } else {
+    return false 
+  }
   
 
 }
@@ -83,7 +108,8 @@ function actualizarPassword(usuario, nuevaPassword) {
   // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevagPassword"
   // Devuelve el objeto
   // Tu código:
-
+ usuario.password = nuevaPassword;
+ return usuario;
 }
 
 function agregarAmigo(usuario, nuevoAmigo) {
@@ -91,7 +117,8 @@ function agregarAmigo(usuario, nuevoAmigo) {
   // Agrega "nuevoAmigo" al final de ese array
   // Devuelve el objeto "usuario"
   // // Tu código:
-  
+  usuario.amigos.push(nuevoAmigo);
+  return usuario
 }
 
 function pasarUsuarioAPremium(usuarios) {
@@ -100,7 +127,10 @@ function pasarUsuarioAPremium(usuarios) {
   // Define cada propiedad "esPremium" de cada objeto como "true"
   // Devuelve el array de usuarios
   // Tu código:
-  
+  for (var i=0; i < usuarios.length; i++){
+    usuarios[i].esPremium =true;
+  }
+  return usuarios;
 }
  
 function sumarLikesDeUsuario(usuario) {
@@ -111,7 +141,12 @@ function sumarLikesDeUsuario(usuario) {
   // Devuelve la suma
   // Tu código:
  
+var sumaLikes = 0;
 
+for (var i = 0 ; i < usuario.posts.length; i++){
+sumaLikes = sumaLikes + usuario.posts[i].likes; 
+}
+return sumaLikes;
 }
 
 function agregarMetodoCalculoDescuento(producto) {
@@ -124,7 +159,11 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
+  producto.calcularPrecioDescuento = function() {
+   return  this.precio - (this.precio * this.porcentajeDeDescuento)
 
+  }
+return producto;
 }
 
 // No modificar nada debajo de esta línea
